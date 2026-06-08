7453546ffecc8ec75699d25fb4f9c800e06dcd74
Gab7Barbosa <gabriel.silva1521@aluno.ce.gov.br>
2026-06-08 12:45:14 -0300

    salvo

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
 $pdo = new PDO(
-    "mysql:host=mysql;dbname=" . getenv('MYSQL_DATABASE') . ";charset=utf8mb4",
-    getenv('MYSQL_USER'),
-    getenv('MYSQL_PASSWORD'),
+    "mysql:host=mysql;dbname=" . getenv('DB_DATABASE') . ";charset=utf8mb4",
+    getenv('DB_USERNAME'),
+    getenv('DB_PASSWORD'),
     [
         PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
